ccae4031cb5dd53fb5afb70185143e5e9afa8a15
Ashish Nagar <ashishnagar033@gmail.com>
2023-08-01 16:52:29 +0530

    operation component created

---
+++ src/components/Operations.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectOperations } from '../features/operationsSlice';
+// import { useDispatch } from 'react-redux';
+
+const Operations = () => {
+  const NumOfoperations = useSelector(selectOperations);
+
+
+  return (
+    <div>
+        {/* this is top */}
+      <div className='p-3 border-2 rounded-lg w-fit mt-10  border-black'>
+        <p className=''>Total operation: <span>{NumOfoperations}</span></p>
+    </div>
+
+    </div>
+  )
+}
+
+export default Operations
