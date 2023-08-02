a7f385447d68abfd1cd8b1987224b1b2c945691e
Ashish Nagar <ashishnagar033@gmail.com>
2023-08-02 10:11:21 +0530

    minor changes code fixes

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setButton } from '../features/ButtonOperation'
+import {  showResult } from '../features/resultSlice'
 
 const Buttons = () => {
+  const emptyInputs = useSelector((state) => state.setInputs.emptyInputs);
 
   const dispatch = useDispatch()
   
+  const handleValue = (button) => {
+
+    dispatch(setButton(button))
+      if(emptyInputs){
+        dispatch(showResult())
+      }
+  }
+  
   return (
     <div className='flex gap-4 justify-center mt-10'>
-    <button className=' border-2 w-10 h-10  rounded-full border-black ' onClick={() => dispatch(setButton('+'))}> + </button>
-    <button className=' border-2 w-10 h-10  rounded-full border-black ' onClick={() =>  dispatch(setButton('-'))}> - </button>
-    <button className=' border-2 w-10 h-10  rounded-full border-black' onClick={() =>  dispatch(setButton('/'))}> / </button>
-    <button className=' border-2 w-10 h-10  rounded-full border-black ' onClick={() =>  dispatch(setButton('*'))}> * </button>
+    <button className=' border-2 w-10 h-10  rounded-full border-black ' onClick={() =>handleValue('+') }> + </button>
+    <button className=' border-2 w-10 h-10  rounded-full border-black ' onClick={() => handleValue('-') }> - </button>
+    <button className=' border-2 w-10 h-10  rounded-full border-black' onClick={() => handleValue('/') }> / </button>
+    <button className=' border-2 w-10 h-10  rounded-full border-black ' onClick={() => handleValue('*') }> * </button>
     </div>
   )
 }
