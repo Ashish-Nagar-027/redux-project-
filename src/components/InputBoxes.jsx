96b84036f93d911ad9db943e6f700d9c8663215b
Ashish Nagar <ashishnagar033@gmail.com>
2023-08-02 10:11:27 +0530

    minor changes code fixes

--- src/components/InputBoxes.jsx
+++ src/components/InputBoxes.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectButton } from '../features/ButtonOperation'
-import { setDefault } from '../features/InputHandlerSlice'
+import { setDefault, setEmptyInputs } from '../features/InputHandlerSlice'
 import { increaseOperationValue } from '../features/operationsSlice'
-import { setResult, showResult } from '../features/resultSlice'
+import { setResult } from '../features/resultSlice'
 
 const InputBoxes = () => {
 
@@ -16,6 +16,9 @@ const InputBoxes = () => {
   const dispatch = useDispatch()
   const resetInput = useSelector((state) => state.setInputs.value);
 
+  const showResult = useSelector((state) => state.result.showResult);
+    
+
 
    useEffect(()=> {
      setInput1("")
@@ -23,21 +26,28 @@ const InputBoxes = () => {
      dispatch(setDefault())
    }, [resetInput])
 
-  const handleValue = () => {
-    if(input1  && input2) {
-      const result = eval(input1+ button + input2)
+  
+   useEffect(() => {
+    if(input1 && input2 ) {
+      dispatch(setEmptyInputs(true))
+    } else(
+      dispatch(setEmptyInputs(false))
+    )
+   }, [input1,input2])
+
+  useEffect(() => {
+     if(showResult){
+      const result = eval(input1 + button + input2)
       dispatch(setResult(result))
       dispatch(increaseOperationValue())
-     dispatch(showResult())
-    }else(
-      alert("please enter some number values in input")
-    )
-  }
+     }
+  } , [showResult,button])
+
 
   return (
        <div className='flex justify-between mt-10'>
         <input type='number' className='max-w-fit border-2 rounded-lg p-2 border-black' placeholder='input-1' value={input1} onChange={(e) => setInput1(e.target.value)}/>
-        <button className=' border-2 w-10 h-10  rounded-full border-black' onClick={handleValue}> {button} </button>
+        <button className=' border-2 w-10 h-10  rounded-full border-black' > {button} </button>
         <input type='number' className='max-w-fit border-2 rounded-lg p-2 border-black' placeholder='input-2' value={input2} onChange={(e) => setInput2(e.target.value)}/>
       </div>
   )
